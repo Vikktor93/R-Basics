d189a3d360fe3a3aa15899cfdc3f66ec509c6879
Vikktor93 <viktorsaldivia93@gmail.com>
2026-02-26 23:45:12 -0300

    Se actualizan los comentarios del fichero

--- Seccion_1_R_Basics/1.3_Data_Types.R
+++ Seccion_1_R_Basics/1.3_Data_Types.R
@@ -18,23 +18,25 @@ str(murders)
 # Mostrando las primeras 6 líneas del conjunto de datos
 head(murders)
 
-# Usando el operador de acceso para obtener la columna population
-murders$population
-# Mostrando los nombres de las variables en el conjunto de datos murders
+# Usando el operador de acceso para obtener la columna population (The Accessor)
+murders$population # El tipo de objeto obtenido es un vector
+# Mostrando los nombres de las variables en el conjunto de datos murders (Nombres de las Columnas)
 names(murders)
+
 # Determinando cuántas entradas hay en un vector
 pop <- murders$population
 length(pop)
+
 # Los vectores pueden ser de clase numérica y de caracteres
 class(pop)
-class(murders$state)
+class(murders$state) # vector de caracteres
 
 # Los vectores lógicos son TRUE o FALSE
 z <- 3 == 2
 z
 class(z)
 
-# Los factores son otro tipo de clase
-class(murders$region)
+# Los factores son otro tipo de clase (Otro tipo de dato)
+class(murders$region) # Factores útil para almacenar Datos Categóricos
 # Obteniendo los niveles de un factor
 levels(murders$region)
